dd39f4bc18adb8f9beec11f5b1d22fdb7231c73e
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2025-12-25 23:57:29 +0100

    Add help dialog with task breakdown explanation

--- app/src/main/java/com/example/focusflow/ThirdActivity.kt
+++ app/src/main/java/com/example/focusflow/ThirdActivity.kt
@@ -1,6 +1,8 @@
 package com.example.focusflow
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,9 +18,14 @@ class ThirdActivity : AppCompatActivity() {
 
         val headerLayout = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.header)
         val backButton = headerLayout.findViewById<TextView>(R.id.backArrow)
+        val helpIcon = findViewById<ImageView>(R.id.helpIcon)
 
         backButton.setOnClickListener {
             finish()
+        }
+
+        helpIcon.setOnClickListener {
+            showHelpDialog()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -26,5 +33,17 @@ class ThirdActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun showHelpDialog() {
+        AlertDialog.Builder(this, R.style.MyDialogTheme)
+            .setMessage("In order to not feel overwhelmed by bigger tasks, we want you to break it into the smallest task possible.\n" +
+                    "Like, open text document or search for one source. How many subtasks you want to create is your choice, but the more the better.")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setCancelable(true)
+            .create()
+            .show()
     }
 }
