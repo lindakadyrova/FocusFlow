fe911249d66db6e76a93987fa959b566cfc2d8e2
Linda Kadyrova <linda.kadyrova@edu.fh-joanneum.at>
2026-01-16 00:28:49 +0100

    Add sixth screen

---
+++ app/src/main/java/com/example/focusflow/SixthActivity.kt
@@ -0,0 +1,35 @@
+package com.example.focusflow
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SixthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sixth)
+
+        val backButton = findViewById<TextView>(R.id.backArrow)
+        val goToPlannedTasksButton = findViewById<Button>(R.id.goToPlannedTasks)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        goToPlannedTasksButton.setOnClickListener {
+
+            finish()
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
